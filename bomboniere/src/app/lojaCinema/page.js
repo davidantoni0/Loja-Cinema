2f9f9e4534945afd405dd476d07d5efec05e2015
Davy Piratao <davidantoniopetro@gmail.com>
2025-06-25 03:07:23 -0300

    Add: some logic and menu buttons of lojaCinema

--- bomboniere/src/app/lojaCinema/page.js
+++ bomboniere/src/app/lojaCinema/page.js
@@ -3,35 +3,72 @@ import { useState } from "react";
 import styles from "./page.module.css"
 
 export default function MenuLoja() {
-    const [number, setNumber] = useState("0");
+    const [step, setStep] = useState("mainMenu");
+    const [selectedItem, setSelectedItem] = useState();
+    const [cart, setCart] = useState([]);
 
-    function handleClick(value) {
-        if (number === "0" && value !== "." && !isNaN(value)) {
-            setNumber(value);
-        } else {
-            setNumber(number + value);
-        }
+    const prices = {
+        Pipocas: { "1": 5.0, "2": 7.5, "3": 10.0 },
+        Refrigerantes: { "1": 4.0, "2": 6.0, "3": 8.0 },
+        Doces: { "1": 2.5, "2": 4.0, "3": 5.5 },
+        Biscoitos: { "1": 3.0, "2": 4.5, "3": 6.0 }
+    };
+
+    function handleClickMenu(item) {
+        setSelectedItem(item);
+        setStep("submenu");
     }
 
+    function handleClickAddItem(size) {
+    const preco = prices[selectedItem][size];
+
+    const newItem = {
+        item: selectedItem,
+        size,
+        preco
+    };
+
+    setCart([...cart, newItem]);
+    setStep("mainMenu");
+    }
+
+    function handleClickCart() {
+
+    }
 
     return(
         <div className={styles.menuLoja}>
             <div className={styles.menuInicio}>Bomboniere</div>
 
-            <div className={styles.keyboard}>
-                <div className={styles.row}>
-                    <button className={styles.button} onClick={() => handleClick("1")}>Pipocas</button>
-                    <button className={styles.button} onClick={() => handleClick("2")}>Refrigerantes</button>
-                    <button className={styles.button} onClick={() => handleClick("3")}>Doces</button>
-                    <button className={styles.button} onClick={() => handleClick("4")}>Biscoitos</button>
+            {step === "mainMenu" && (
+
+            <div className={styles.row}>
+                <button className={styles.button} onClick={() => handleClickMenu("Pipocas")}>Pipocas</button>
+                <button className={styles.button} onClick={() => handleClickMenu("Refrigerantes")}>Refrigerantes</button>
+                <button className={styles.button} onClick={() => handleClickMenu("Doces")}>Doces</button>
+                <button className={styles.button} onClick={() => handleClickMenu("Biscoitos")}>Biscoitos</button>
+            </div>
+            )}
+            
+            {step === "submenu" && (
+            <div className={styles.submenu}>
+            <div className={styles.submenuTitle}>
+                Selecione o tamanho de {selectedItem}:
+            </div>
+            <button className={styles.button} onClick={() => handleClickAddItem("1")}>Pequeno</button>
+            <button className={styles.button} onClick={() => handleClickAddItem("2")}>Médio</button>
+            <button className={styles.button} onClick={() => handleClickAddItem("3")}>Grande</button>
+            </div>
+            )}
+
+            <footer className={styles.footer}>
+                <div className={styles.cart}>
+                <button className={styles.button} onClick={() => handleClickCart("Carrinho")}>Veja o Carrinho ({cart.length} itens)</button>
                 </div>
 
-            </div>
-            
-            <footer className={styles.footer}>
-                <a>
-                    veja 
-                </a>
+                <div className={styles.deleteCart}>
+                <button className={styles.button} onClick={() => handleClickCart("Carrinho")}>Veja o Carrinho ({cart.length} itens)</button>
+                </div>
             </footer>
         </div>
     );
