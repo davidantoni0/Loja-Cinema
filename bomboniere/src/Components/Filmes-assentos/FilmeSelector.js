ca335b55d2b4a1f978ad91d67a42a6b32ec576e5
davidantoni0 <davidantoniopetro@gmail.com>
2025-06-30 15:09:25 -0300

    Add: styles to filme and assentos buttons

--- bomboniere/src/Components/Filmes-assentos/FilmeSelector.js
+++ bomboniere/src/Components/Filmes-assentos/FilmeSelector.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-
+import styles from './FilmeSelector.module.css';
 const FilmeSelector = ({ filmes, onSelectFilme }) => {
   return (
     <div>
       <h2>Selecione um Filme</h2>
       <div>
         {filmes.map(filme => (
-          <button key={filme.id} onClick={() => onSelectFilme(filme)}>
+          <button className={styles.filmeButton} key={filme.id} onClick={() => onSelectFilme(filme)}>
             {filme.nome}
           </button>
         ))}
